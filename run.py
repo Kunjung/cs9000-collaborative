844e753b14eef1e777d2d221a39d49cd69bba867
Kunjung Sherpa <kunjungsherpa@gmail.com>
2017-06-11 23:07:54 +0545

    Create run.py

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 from app import app
 
-import os
-port = int(os.environ.get("PORT", 5000))
-app.run(debug=True, host='0.0.0.0', port=port)
+#import os
+#port = int(os.environ.get("PORT", 5000))
+#app.run(debug=True, host='0.0.0.0', port=port)
+app.run()

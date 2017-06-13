5a5f1d47c762d52e4473fa428f70d67aaf922ac4
Kunjung Sherpa <kunjungsherpa@gmail.com>
2017-06-13 23:43:24 +0545

    Create ML.py

--- app/ML.py
+++ app/ML.py
@@ -1,7 +1,5 @@
 # Mid-course Happiness Score: 23
 from .models import *
-from math import sqrt
-
 
 def get_rating(user_id, movie_id):
 	query = ancient_ratings.select('rating').where(ancient_ratings.c.user_id==user_id).where(ancient_ratings.c.movie_id==movie_id)
@@ -15,12 +13,6 @@ def get_all_users():
 def get_all_movies():
 	return Movie.query.all()
 
-def get_movies(user_id):
-	query = ancient_ratings.select('movie_id').where(ancient_ratings.c.user_id==user_id)
-	values = db.session.execute(query).all()
-	user_movies = values[1]
-	return user_movies
-
 # Returns a distance-based similarity score for person1 and person2
 def sim_distance(user1, user2):
 	# Get the list of shared_movies
@@ -29,6 +21,7 @@ def sim_distance(user1, user2):
 	user1_movies = user1.rated
 	user2_movies = user2.rated
 
+	sum_of_squares = 0
 	for movie in user1_movies:
 		if movie in user2_movies:
 			si[movie.id]=1
@@ -37,13 +30,13 @@ def sim_distance(user1, user2):
 		if len(si)==0: return 0
 
 		#Get the ratings for the particular movie
-		user1_ratings = get_rating(user1.id, movie.id)
-		user2_ratings = get_rating(user2.id, movie.id)
+		user1_rating = get_rating(user1.id, movie.id)
+		user2_rating = get_rating(user2.id, movie.id)
 
 		# Add up the squares of all the differences
-		sum_of_squares=sum([pow(user1_ratings - user2_ratings, 2)
-					for movie1 in user1_movies if movie1 in user2_movies])
-
+		diff = user1_rating - user2_rating
+		sum_of_squares = sum_of_squares + diff * diff
+					
 return 1/(1+sum_of_squares)
 
 
@@ -60,11 +53,12 @@ def predict_movies_for_user(user, users, n=20):
 		sim = similarity_distance(user, other)
 
 		# ignore scores of zero or lower
-		if sim <=0: continue
+		if sim <= 0:
+			continue
 		for movie in other.rated:
 
 			# only score movies I haven't seen yet
-			if movie not in user.rated ==0:
+			if movie not in user.rated:
 
 				# Similarity * Score
 				totals.setdefault(movie.id ,0)
